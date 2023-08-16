9c3726d85ffb1e5d361047fe28de9694c28631d2
Griffia <griffia@hotmail.co.uk>
2023-08-16 03:09:20 +0100

    Added Poet Name
    Added an additional array to then randomly generate an author of the poem.

--- haikuGenerator.js
+++ haikuGenerator.js
@@ -2,14 +2,17 @@ const generateHaiku = () => {
     const firstLine = ['The wren,', 'From time to time,', 'Over-ripe sushi,', 'Consider me,', 'Blowing from the west,', 'In the cicada\'s cry,'];
     const secondLine = ['Earns his living,', 'The clouds give rest,', 'The Master,', 'As one who loved poetry,', 'No sign can foretell,'];
     const thirdLine = ['Noiselessly.', 'To the moon-beholders.', 'Is full of regret.', 'And persimmons.', 'How soon it must die.', 'In the east.'];
+    const names = ['Kobayahsi Issa', 'Matsuo Bashō', 'Yosa Buson', 'Masaoaka Shiki']
     // Arrays containing all possible combinations 
-    
+
+
     firstRandomOption = Math.floor(Math.random() * firstLine.length);
     secondRandomOption = Math.floor(Math.random() * secondLine.length);
     thirdRandomOption = Math.floor(Math.random() * thirdLine.length);
+    namesOption = Math.floor(Math.random() * names.length);
     // Randomly generates an index for each array
 
-    return console.log(`${firstLine[firstRandomOption]} \n${secondLine[secondRandomOption]} \n${thirdLine[thirdRandomOption]} \n -Billy Bob`);
+    return console.log(`${firstLine[firstRandomOption]} \n${secondLine[secondRandomOption]} \n${thirdLine[thirdRandomOption]} \n-${names[namesOption]}`);
     // returns the haiku to the user using the array and the indexs
 }   
 
